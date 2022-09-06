0334d4dd1940f1901523f3a40f6b3c3be4a8ca3a
Anis <mohamedrbh519@gmail.com>
2022-09-06 10:33:42 +0100

    contenu de la balise p modifié

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log('Hello world');
 
 document.querySelector('h1').textContent = 'This is a title';
+
+document.getElementsByTagName('p')[0].innerHTML = `<p>
+                                                        <span>Texte modifié</span>
+                                                        </p>`;
